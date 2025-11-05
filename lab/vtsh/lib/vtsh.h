f8ed96cbccc3e5ba0d6aa9f13fabd352c6f321b3
down-shift <anicegoose@gmail.com>
2025-11-05 23:53:35 +0300

    base shell done

--- lab/vtsh/lib/vtsh.h
+++ lab/vtsh/lib/vtsh.h
@@ -1,3 +1,14 @@
 #pragma once
 
+#define BUFFER_SIZE 1024
+#define MAX_ARGS 64
+
 const char* vtsh_prompt();
+
+int read_line(char* buffer, int max_len);
+
+int parse_line(char* line, char** args, int max_args);
+
+int execute(char** args);
+
+int execute_multiple(const char* input);
