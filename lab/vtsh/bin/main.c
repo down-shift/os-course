fd43f275a647863bb9c98c365abe82cbbaa0291c
down-shift <anicegoose@gmail.com>
2025-11-07 11:08:18 +0300

    part 2 done

--- lab/vtsh/bin/main.c
+++ lab/vtsh/bin/main.c
@@ -1,41 +1,36 @@
-#include "vtsh.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#include "vtsh.h"
+
 int main(void) {
-    char buffer[BUFFER_SIZE];
-    char* args[MAX_ARGS];
+  char buffer[BUFFER_SIZE];
+  int is_interactive = isatty(STDIN_FILENO);
 
-    int is_interactive = isatty(STDIN_FILENO);
-
-    while (1) {
-        if (is_interactive) {
-            printf("%s", vtsh_prompt());
-            // fflush(stdout);
-        }
-
-        if (!read_line(buffer, BUFFER_SIZE)) {
-            break;  // EOF
-        }
-
-        // Remove trailing newline
-        size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n') {
-            buffer[len - 1] = '\0';
-        }
-
-        // Skip empty lines
-        if (strlen(buffer) == 0) {
-            continue;
-        }
-
-        int num_args = parse_line(buffer, args, MAX_ARGS);
-        if (num_args > 0) {
-            execute(args);
-        }
+  while (1) {
+    if (is_interactive) {
+      printf("%s", vtsh_prompt());
+      (void)fflush(stdout);
     }
 
-    return EXIT_SUCCESS;
+    if (!read_line(buffer, BUFFER_SIZE)) {
+      break;
+    }
+
+    size_t len = strlen(buffer);
+    if (len > 0 && buffer[len - 1] == '\n') {
+      buffer[len - 1] = '\0';
+    }
+
+    if (strlen(buffer) == 0) {
+      continue;
+    }
+
+    execute(buffer);
+  }
+
+  return EXIT_SUCCESS;
 }
--- lab/vtsh/lib/vtsh.h
+++ lab/vtsh/lib/vtsh.h
@@ -2,6 +2,20 @@
 
 #define BUFFER_SIZE 1024
 #define MAX_ARGS 64
+#define MAX_COMMANDS 50
+
+typedef enum {
+    CMD_NONE,
+    CMD_AND,
+    CMD_OR,
+    CMD_SEQ,
+    CMD_BG
+} cmd_operator_t;
+
+typedef struct {
+    char* args[MAX_ARGS];
+    cmd_operator_t next_op;
+} command_t;
 
 const char* vtsh_prompt();
 
@@ -9,6 +23,6 @@ int read_line(char* buffer, int max_len);
 
 int parse_line(char* line, char** args, int max_args);
 
-int execute(char** args);
+int execute(char* line);
 
 int execute_multiple(const char* input);
