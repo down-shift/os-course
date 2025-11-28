7b558b32a1c97e29783f927aed379e2c03cf7462
down-shift <anicegoose@gmail.com>
2025-11-28 12:01:14 +0300

    revert 4 commits

--- lab/vtsh/bin/main.c
+++ lab/vtsh/bin/main.c
@@ -1,35 +1,7 @@
-#include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-#include "vtsh.h"
+#include <vtsh.h>
 
 int main() {
-  char buffer[BUFFER_SIZE];
-
-  vtsh_init();
-
-  while (1) {
-    printf("%s", vtsh_prompt());
-    (void)fflush(stdout);
-
-    if (!read_line(buffer, BUFFER_SIZE)) {
-      break;
-    }
-
-    size_t len = strlen(buffer);
-    if (len > 0 && buffer[len - 1] == '\n') {
-      buffer[len - 1] = '\0';
-    }
-
-    if (strlen(buffer) == 0) {
-      continue;
-    }
-
-    execute(buffer);
-  }
-
-  return EXIT_SUCCESS;
+  printf("%s", vtsh_prompt());
+  printf("Hello, world!\n");
 }
--- lab/vtsh/lib/vtsh.h
+++ lab/vtsh/lib/vtsh.h
@@ -1,29 +1,3 @@
 #pragma once
 
-#define BUFFER_SIZE 1024
-#define MAX_ARGS 64
-#define MAX_COMMANDS 50
-
-typedef enum {
-    CMD_NONE,
-    CMD_AND,
-    CMD_OR,
-    CMD_SEQ,
-    CMD_BG,
-    CMD_PIPE
-} cmd_operator_t;
-
-typedef struct {
-    char* args[MAX_ARGS];
-    cmd_operator_t next_op;
-} command_t;
-
 const char* vtsh_prompt();
-
-int read_line(char* buffer, int max_len);
-
-int parse_line(char* line, char** args, int max_args);
-
-int execute(char* line);
-
-void vtsh_init(void);
